080ea927b0bdd3b1ed0ffde58b504862e2e2736a
ahmed-sedky <61352734+ahmed-sedky@users.noreply.github.com>
2021-01-03 01:00:17 +0200

    4th commit admin's part

--- main.py
+++ main.py
@@ -14,22 +14,24 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def Aindex():
-    myCursor.execute("SELECT count(id) FROM dr")
+
+    myCursor.execute("SELECT id FROM dr")
     myResult3 = myCursor.fetchall()
 
     myCursor2 = mydb.cursor()
-    myCursor2.execute("SELECT count(ssn) FROM patient")
+    myCursor2.execute("SELECT ssn FROM patient")
     myResult4 = myCursor2.fetchall()
     
     myCursor3 = mydb.cursor()
-    myCursor3.execute("SELECT count(*) FROM contact_us")
+    myCursor3.execute("SELECT * FROM contact_us")
     myResult6 = myCursor3.fetchall()
-    return render_template('Aindex.html', doctors_no = myResult3 ,p_no =myResult4,comp_no =myResult6)
+    return render_template('Aindex.html', doctors_no = myCursor.rowcount ,p_no =myCursor2.rowcount,comp_no =myCursor3.rowcount)
 
 
-@app.route('/Adoctor')
+@app.route('/Adoctor',methods = ['POST', 'GET'])
 def Adoctor():
-    myCursor.execute("SELECT * FROM dr")
+
+    myCursor.execute("SELECT id,dr.uname,dr.pass,dr.email,dr.phone,dr.address,dep_name,exp_years,patient.uname FROM dr JOIN dep on dr.depno = dep.dep_no JOIN dr_patient on dr_patient.dr_id =dr.id JOIN patient ON patient.ssn =dr_patient.patient_ssn")
     myResult = myCursor.fetchall()
     return render_template('Adoctor.html' ,doctors_data = myResult)
 
@@ -39,16 +41,22 @@ def Dedit():
 
 @app.route('/Ddelete')
 def Ddelete():
-    sql = "INSERT INTO doctors (name,departement,id) VALUES(%s,%s,%s)"
-    value = (name ,departement,id)
-    myCursor.execute(sql,value)
-    mydb.commit()
-    return render_template('Doctor.html')
+    doc_id = request.args.get("dr_id")
+
+    # sql = "DELETE FROM dr WHERE id = %s"
+    # id = (doc_id, )
+
+    # mycursor.execute(sql, id)
+
+    # mydb.commit()
+
+    print(doc_id, "   record(s) deleted")
+    return render_template('Adoctor.html')
 
 
 @app.route('/Apatient') 
 def Apatient():
-    myCursor.execute("SELECT * FROM patient")
+    myCursor.execute("SELECT ssn , patient.uname , patient.pass , patient.email , patient.phone , patient.address , patient.weight ,patient.entry_day , patient.disease , dr.uname FROM patient JOIN dr_patient on dr_patient.patient_ssn =patient.ssn JOIN dr ON dr.id =dr_patient.dr_id")
     myResult2 = myCursor.fetchall()
     return render_template('Apatient.html' , patients_data = myResult2)
 
--- static/script/main.js
+++ static/script/main.js
@@ -11,3 +11,80 @@ window.onload =function(){
         box3.classList.add("animate__zoomIn")
     },2000);
 }
+$(function(){
+    'use strict';
+    $('[placeholder]').focus(function(){
+        $(this).attr('data-text',$(this).attr('placeholder'));
+        $(this).attr('placeholder','');
+    }).blur(function(){
+        $(this).attr('placeholder',$(this).attr('data-text'));
+    });
+    $('input').each(function(){    //add asterisk
+        if($(this).attr('required')==='required'){
+            $(this).after("<span class='asterisk'> * </span>");
+        }
+    }); 
+    var show_field = $(".password");
+    $(".show-pass").hover(function(){
+        show_field.attr('type','text');
+    },function(){
+        show_field.attr('type','password');
+    });
+    $(".confirm").click(function(){
+        return confirm("Are You Sure?");
+    });
+
+    $('.toggle-info').click(function(){
+        console.log("Yes");
+        $(this).toggleClass("selected").parent().next(".panel-body").fadeToggle(200);
+        if($(this).hasClass("selected")){
+            $(this).html("<i class='fas fa-plus'></i>");
+        }else{
+            $(this).html("<i class='fas fa-minus'></i>");
+        }
+    });
+    function keyUp($input,$class){
+        $($input).on("keyup",function(){
+            $($class).text($(this).val());
+        });
+    }
+    keyUp(".live-name",".live-ad .caption h5");
+    keyUp(".live-desc",".live-ad .caption p");
+    keyUp(".live-price",".live-ad .price_tag");
+    keyUp(".live-country",".live-ad .caption .country-tag");
+
+    
+    $(".live-name").on("keyup",function(){
+        $(".live-ad .caption h5").text($(this).val());
+    });
+    $(".heart").click(function(){
+        $(this).toggleClass("like");
+        if($(this).hasClass("like")){
+            $(this).html("<i class='fas fa-heart heart2'></i>");
+        }else{
+            $(this).html("<i class='far fa-heart heart2'></i>");
+        }
+    })
+    $(".heart3").click(function(){
+        $(this).toggleClass("like");
+        if($(this).hasClass("like")){
+            $(this).html("<i class='fas fa-heart heart2'></i>");
+        }else{
+            $(this).html("<i class='far fa-heart heart2'></i>");
+        }
+    })
+    $(".errorMsg").click(function(e){
+        e.preventDefault();
+        $(".Msg").fadeIn();
+    });
+    $(".Msg").click(function(){
+        $(this).fadeOut();
+    });
+    $(".imgWish").mouseover(function(){
+        $(".Msg2").fadeIn();
+    });
+    $(".imgWish").mouseleave(function(){
+        $(".Msg2").fadeOut();
+    });
+    
+}); 
